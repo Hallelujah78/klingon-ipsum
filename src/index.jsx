22944d59bb3aa66d5e153690c0d734313274c432
winklevi1 <gavan.browne@gmail.com>
2023-10-31 00:04:20 +0000

    Project Update.
    Added vite-plugin-pwa.

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+// import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 import App from "./App";
 
@@ -11,4 +11,4 @@ root.render(
   </React.StrictMode>
 );
 
-serviceWorkerRegistration.register();
+// serviceWorkerRegistration.register();
