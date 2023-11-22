4e495d31769c388a54e0fd973a1913877cd2e882
Hallelujah78 <gavan.browne@gmail.com>
2023-11-22 15:53:48 +0000

    Project Update.
    Added renderComponent to App.test.js, added a test for input.

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,11 @@ import { setupServer } from "msw/node";
 import { http, HttpResponse } from "msw";
 import klingonWords from "./data/data.jsx";
 
+const renderComponent = async () => {
+  render(<App />);
+  await screen.findByRole("button");
+};
+
 jest.mock("uuid", () => {
   const base = "9134e286-6f71-427a-bf00-";
   let current = 100000000000;
@@ -38,13 +43,29 @@ afterEach(() => {
 afterAll(() => {
   server.close();
 });
+// is loading
 
-test("a simple test", async () => {
-  render(<App />);
-  await pause();
+// !isLoading && !isError && !showProgress
+// a button is visible
+// an input is visible
+// a progressbar is not visible
+
+// !isLoading && !isError && showProgress
+// a progressbar is visible
+
+test.only("an input is visible", async () => {
+  await renderComponent();
+  screen.debug();
+  const input = await screen.findByRole("spinbutton");
+
+  expect(input).toBeInTheDocument();
+});
+
+test("a button is visible", async () => {
+  await renderComponent();
+  screen.debug();
   const button = await screen.findByRole("button");
   expect(button).toBeInTheDocument();
-  screen.debug();
 });
 
 const pause = () => {
