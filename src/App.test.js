19848ae1afba3c766f8315d91b64de2318edc8ac
winklevi1 <gavan.browne@gmail.com>
2023-11-28 23:44:55 +0000

    Project Update.
    Added testString.js, a string with 2000 words that is used to check that Progress.jsx is rendered. Added mock for generateWordArray that returns testString split into array. Added three more tests to App.test.js.

--- src/App.test.js
+++ src/App.test.js
@@ -5,6 +5,18 @@ import createServer from "./test/server.js";
 import { http, HttpResponse } from "msw";
 import klingonWords from "./data/data.jsx";
 
+import { mockTestString } from "./test/testSting.js";
+
+jest.mock("./utils/generateWordArray.js", () => {
+  return {
+    generateWordArray: () => {
+      const tempString = mockTestString;
+
+      return tempString.split();
+    },
+  };
+});
+
 const renderComponent = async () => {
   render(<App />);
   await screen.findByRole("spinbutton");
@@ -99,11 +111,54 @@ describe("after data has been fetched", () => {
     expect(alert).toHaveTextContent("please enter a positive number");
   });
 
-  test("if the user attempts to enter nonnumeric values in the input, the value is not updated", async () => {
+  test("if the user enters a negative number in the input and attempts to generate paragraphs, a warning is displayed", async () => {
     await renderComponent();
     const input = await screen.findByRole("spinbutton");
+    const button = await screen.findByRole("button", {
+      name: /i want that thing!/i,
+    });
+
     await user.click(input);
-    await user.keyboard("abcdefg");
-    expect(input).toHaveValue(null);
+    await user.keyboard("-20");
+    await user.click(button);
+
+    const alert = await screen.findByTestId("alert");
+
+    expect(input).toHaveValue(-20);
+    expect(alert).toBeInTheDocument();
+    expect(alert).toHaveTextContent("please enter a positive number");
+  });
+
+  test("if the user enters 0 in the input and attempts to generate paragraphs, a warning is displayed", async () => {
+    await renderComponent();
+    const input = await screen.findByRole("spinbutton");
+    const button = await screen.findByRole("button", {
+      name: /i want that thing!/i,
+    });
+
+    await user.click(input);
+    await user.keyboard("0");
+    await user.click(button);
+
+    const alert = await screen.findByTestId("alert");
+
+    expect(input).toHaveValue(0);
+    expect(alert).toBeInTheDocument();
+    expect(alert).toHaveTextContent("please enter a positive number");
+  });
+
+  test.only("if the user enters a positive number in the input and attempts to generate paragraphs, a progress bar is displayed", async () => {
+    await renderComponent();
+
+    const input = await screen.findByRole("spinbutton");
+    const button = await screen.findByRole("button", {
+      name: /i want that thing!/i,
+    });
+
+    await user.click(input);
+    await user.keyboard("2000");
+    await user.click(button);
+    const progressbar = await screen.findByRole("progressbar");
+    expect(progressbar).toBeInTheDocument();
   });
 });
