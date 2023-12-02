9cc72dd2164fe7e4d44741679205b94d9e0bc575
winklevi1 <gavan.browne@gmail.com>
2023-12-02 18:23:23 +0000

    Project Update.
    Added test for generateWordArray to utils.test.js.

--- src/utils/utils.test.js
+++ src/utils/utils.test.js
@@ -1,7 +1,21 @@
 import { generateRandomNumber } from "./utils";
+import { generateWordArray } from "./generateWordArray";
+
+const mockSetProgress = jest.fn();
 
 describe("generateRandomNumber", () => {
-  test("test", () => {
+  test("when provided with an array of elements, returns a number which is an index value", () => {
     expect([0, 1, 2, 3]).toContain(generateRandomNumber(["a", "b", "c", "d"]));
+  });
+});
+
+describe("generateWordArray", () => {
+  test("when provided with a function to update progress, a dictionary of words as an array, and a number of words - setProgress, dictionary, words - it returns 'text' which is an array that contains N words where N is equal to the value of 'words'", async () => {
+    const array = await generateWordArray(
+      mockSetProgress,
+      ["a", "b", "c", "d"],
+      2
+    );
+    expect(array).toHaveLength(2 * 2);
   });
 });
