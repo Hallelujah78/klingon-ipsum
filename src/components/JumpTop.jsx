c7b3deaf3a39e8a8997fccfc7f976307da32ea97
winklevi1 <gavan.browne@gmail.com>
2023-12-25 18:47:00 +0000

    Finalized JumpTop component.
    JumpTop, scroll to top/jump to top of page functionality, is now complete and appears to be working as intended. Easy implementation!

--- src/components/JumpTop.jsx
+++ src/components/JumpTop.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { FaChevronCircleUp } from "react-icons/fa";
+import styled from "styled-components";
 
 const JumpTop = () => {
   const getYOffset = () => {
@@ -19,11 +20,32 @@ const JumpTop = () => {
     };
   }, []);
   return (
-    <FaChevronCircleUp
-      onClick={() => window.scrollTo({ top: 0, left: 0, behavior: "smooth" })}
-      className="button-top hide"
-    />
+    <Wrapper>
+      <FaChevronCircleUp
+        onClick={() => window.scrollTo({ top: 0, left: 0, behavior: "smooth" })}
+        className="button-top hide"
+      />
+    </Wrapper>
   );
 };
 
 export default JumpTop;
+
+const Wrapper = styled.div`
+  .button-top {
+    z-index: 99;
+    position: fixed;
+    top: 70vh;
+    right: 0.5rem;
+    color: rgba(240, 240, 240);
+    background-color: var(--primary-500);
+    border-radius: 50%;
+    transition: all 0.3s linear;
+    cursor: pointer;
+    font-size: 3rem;
+  }
+  .hide {
+    opacity: 0;
+    z-index: -99;
+  }
+`;
